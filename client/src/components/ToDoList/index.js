dad51f28386e8d74a6bb45389eadc823562550b2
Megan Swenson <mrsmeganswenson@gmail.com>
2021-01-11 14:42:28 -0700

    Created column names for to do list

--- client/src/components/ToDoList/index.js
+++ client/src/components/ToDoList/index.js
@@ -6,6 +6,11 @@ function ToDoList() {
     return (
         <Card id="todo-list" className="text-center">
             <h1>TO DO</h1>
+            <div className="todo-columns text-center">
+                <h5 className="m-2">Title</h5>
+                <h5 className="m-2">Priority</h5>
+                <h5 className="m-2">Notes</h5>
+            </div>
             <div id="current-todos">
                 
             </div>
